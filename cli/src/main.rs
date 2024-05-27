9944c57e70362bb0536118f226a2e20f729d49fa
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2024-05-27 20:51:05 +1000

    feat: implement push command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,22 @@
 use clap::{Parser, Subcommand, Args, ValueEnum};
+use anyhow::{Context, Result};
+use serde::{Deserialize,Serialize};
+use serde_json::json;
+use base64::prelude::*;
+use reqwest::{Client, header::CONTENT_TYPE};
+
 /// Manages environment helping saving and pulling it
 #[derive(Parser, Debug)]
 pub struct App {
     #[clap(subcommand)]
     command: Command
 }
+
+#[derive(Deserialize, Debug)]
+struct PushResponse {
+    message: String
+}
+
 
 #[derive(Subcommand, Debug)]
 enum Command {
@@ -15,7 +27,7 @@ enum Command {
     Push(RepoActionEnvArgs)
 }
 
-#[derive(Clone, ValueEnum, Debug)]
+#[derive(Clone, ValueEnum, Debug, Serialize)]
 enum Environment {
     Dev,
     Qa,
@@ -24,7 +36,10 @@ enum Environment {
 
 #[derive(Args, Debug)]
 struct RepoActionEnvArgs {
-    /// Repository where to find the .env file
+    /// The organization that owns the repository. Ensure that you have the necessary access permissions.
+    org: String,
+
+    /// The specific repository within the given organization where the `.env` file is located.
     repository: String,
 
     /// Environment where to find the .env file
@@ -33,20 +48,47 @@ struct RepoActionEnvArgs {
 
 
 
-fn main() {
+fn main() -> Result<()> {
     let cli = App::parse();
 
     match cli.command {
-        Command::Pull(value) => { pull_handler(value) }
-        Command::Push(value) => { push_handler(value) }
+        Command::Pull(value) => { pull_handler(value)? }
+        Command::Push(value) => { push_handler(value)? }
     }
 
+
+    Ok(())
 }
 
-fn pull_handler(value: RepoActionEnvArgs) {
+#[tokio::main]
+async fn pull_handler(value: RepoActionEnvArgs)-> Result<()>{
     println!("Pull: {:?}", value);
+
+    Ok(())
 }
 
-fn push_handler(value: RepoActionEnvArgs) {
-    println!("Push: {:?}", value);
+#[tokio::main]
+async fn push_handler(value: RepoActionEnvArgs) -> Result<()> {
+    let path = "./.env";
+    let content = std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
+    let request_url = "https://t5m17jo2d8.execute-api.ap-southeast-2.amazonaws.com/dev/sendPushEnv"; // TODO: Convert to environment variable
+    let request_body = json!({
+        "org": value.org,
+        "repo": value.repository,
+        "env": value.env,
+        "b64String": BASE64_STANDARD.encode(content)
+    });
+    let response = Client::new()
+    .post(request_url)
+    .header(CONTENT_TYPE, "application/json")
+    .json(&request_body)
+    .send().await?;
+
+    response.error_for_status_ref()?;
+    
+    let result: PushResponse = response.json().await?;
+    
+    println!("{}", result.message);
+
+    Ok(())
 }
