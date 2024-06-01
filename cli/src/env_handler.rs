9f396842e255322c8b30f2c2d6a3557cd7583d08
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2024-06-01 19:29:26 +1000

    refactor: split into files

---
+++ cli/src/env_handler.rs
@@ -0,0 +1,54 @@
+use crate::cli_struct::{PullResponse, PushResponse, RepoActionEnvArgs};
+use anyhow::{Context, Result};
+use base64::prelude::*;
+use reqwest::{header::CONTENT_TYPE, Client};
+use serde_json::json;
+use std::fs::File;
+use std::io::Write;
+
+#[tokio::main]
+pub async fn pull_handler(value: RepoActionEnvArgs) -> Result<()> {
+    let path = "./.env"; // TODO: Turn path as an optional field
+    let request_url = format!("https://t5m17jo2d8.execute-api.ap-southeast-2.amazonaws.com/dev/sendPullEnv?org={}&repo={}&env={}", value.org, value.repository, value.env);
+    let response = Client::new().get(request_url).send().await?;
+
+    response.error_for_status_ref()?;
+
+    let result: PullResponse = response.json().await?;
+    let env = BASE64_STANDARD
+        .decode(result.file)
+        .expect("Failed to decode base64 data");
+    let mut file = File::create(path)?;
+
+    file.write_all(&env)?;
+
+    Ok(())
+}
+
+#[tokio::main]
+pub async fn push_handler(value: RepoActionEnvArgs) -> Result<()> {
+    let path = "./.env"; // TODO: Turn path as an optional field
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
+    let request_url = "https://t5m17jo2d8.execute-api.ap-southeast-2.amazonaws.com/dev/sendPushEnv"; // TODO: Convert it to environment variable
+    let request_body = json!({
+        "org": value.org,
+        "repo": value.repository,
+        "env": value.env,
+        "b64String": BASE64_STANDARD.encode(content)
+    });
+    let response = Client::new()
+        .post(request_url)
+        .header(CONTENT_TYPE, "application/json")
+        .json(&request_body)
+        .send()
+        .await?;
+
+    response.error_for_status_ref()?;
+
+    let result: PushResponse = response.json().await?;
+
+    println!("{}", result.message);
+
+    Ok(())
+}
